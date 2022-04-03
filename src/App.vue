d3e79eb19a1b7fd366ed1d959a5daa42393b27fd
sundlee <sundlee@gmail.com>
2022-04-04 07:47:27 +0900

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <MonacoEditor
+      width="800"
+      height="500"
+      theme="vs"
+      language="javascript"
+      :options="options"
+      v-model="code"
+      @change="onChange"
+    ></MonacoEditor>
+  </div>
+</template>
+
+<script>
+import MonacoEditor from 'monaco-editor-vue-fixed';
+
+export default {
+  name: 'App',
+  components: {
+    MonacoEditor,
+  },
+
+  data() {
+    return {
+      code: 'const noop = () => {\nconsole.log(\'Hello World!\');\n};',
+      options: {
+        //Monaco Editor Options
+      },
+    };
+  },
+  methods: {
+    onChange(value) {
+      console.log(value);
+    }
+  },
+}
+</script>
+
+<style>
+.editor {
+  width: 600px;
+  height: 800px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('@vue/cli-service');
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['javascript', 'css', 'html', 'typescript']
+      })
+    ]
+   }
+});
